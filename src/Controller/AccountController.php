f5150dc9bbc670c2ea74127faac7e3ae6f21661a
cs-development-github <cs.communication.dev@gmail.com>
2025-02-03 10:26:02 +0100

    update: user entity

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,17 +2,23 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class AccountController extends AbstractController
 {
     #[Route('/account', name: 'app_account')]
-    public function index(): Response
+    public function index(Security $security): Response
     {
+
+        $user = $security->getUser();
+
+        if(!$user) return $this->redirectToRoute('app_login');
+
         return $this->render('account/index.html.twig', [
-            'controller_name' => 'AccountController',
+            'user' => $user
         ]);
     }
 }
