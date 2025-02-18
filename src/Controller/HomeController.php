ea90907362f4c4ea03d82aa3736c9f8603e50dce
cs-development-github <cs.communication.dev@gmail.com>
2025-02-18 11:37:27 +0100

    add: intervention on homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,15 +1,26 @@
 <?php
+
 namespace App\Controller;
 
+use App\Repository\InterventionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    #[IsGranted('ROLE_USER')] // S'assure que l'utilisateur est connecté
+    public function index(InterventionRepository $interventionRepository): Response
     {
-        return $this->render('home/index.html.twig');
+        $user = $this->getUser();
+
+        // Récupérer la prochaine intervention de l'utilisateur
+        $nextIntervention = $interventionRepository->findNextInterventionByUser($user);
+
+        return $this->render('home/index.html.twig', [
+            'nextIntervention' => $nextIntervention,
+        ]);
     }
 }
