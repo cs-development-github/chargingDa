5a7d26a3ded0d0a6d358f07b471cc978b75d229a
cs-development-github <cs.communication.dev@gmail.com>
2025-02-25 10:45:45 +0100

    add: a2f for client

--- migrations/Version20250225093117.php
+++ migrations/Version20250225093117.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250224130957 extends AbstractMigration
+final class Version20250225093117 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20250224130957 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE client DROP price_kwh, DROP price_resale');
+        $this->addSql('ALTER TABLE client ADD otp_code VARCHAR(6) DEFAULT NULL, ADD otp_expires_at DATETIME DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE client ADD price_kwh VARCHAR(255) DEFAULT NULL, ADD price_resale VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE client DROP otp_code, DROP otp_expires_at');
     }
 }
--- src/Repository/ChargingStationSettingRepository.php
+++ src/Repository/ChargingStationSettingRepository.php
@@ -15,29 +15,4 @@ class ChargingStationSettingRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, ChargingStationSetting::class);
     }
-
-    //    /**
-    //     * @return ChargingStationSetting[] Returns an array of ChargingStationSetting objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('c')
-    //            ->andWhere('c.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('c.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
-
-    //    public function findOneBySomeField($value): ?ChargingStationSetting
-    //    {
-    //        return $this->createQueryBuilder('c')
-    //            ->andWhere('c.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
 }
