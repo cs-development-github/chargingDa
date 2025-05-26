d108690b348b2973c9e61fc871ef1ab632275f48
cs-development-github <cs-development-github@gmail.com>
2025-05-26 20:11:16 +0200

    fix: new logic for config flotte

--- src/Form/ConfigFlotteTpe.php
+++ src/Form/ConfigFlotteTpe.php
@@ -3,7 +3,7 @@
 namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\FormBuilderInterface;
 
 class ConfigFlotteType extends AbstractType
@@ -11,7 +11,7 @@ class ConfigFlotteType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('termsAccepted', CheckboxType::class, [
+            ->add('termsAccepted', HiddenType::class, [
                 'label' => 'J’ai lu et j’accepte',
                 'required' => true
             ]);
