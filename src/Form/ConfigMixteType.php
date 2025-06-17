22e56ab0932f159abe13cea8802985f37e45a7a0
cs-development-github <cs.communication.dev@gmail.com>
2025-06-17 16:04:19 +0200

    add: ac dc condition

--- src/Form/ConfigMixteType.php
+++ src/Form/ConfigMixteType.php
@@ -4,7 +4,6 @@ namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,55 +11,72 @@ class ConfigMixteType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder
-            ->add('prix_collab', NumberType::class, [
-                'label' => 'Prix :',
-                'required' => true,
-                'scale' => 2,
-                'attr' => ['class' => 'styled-input'],
-            ])
-            ->add('prix_public', NumberType::class, [
-                'label' => 'Prix : ',
-                'required' => false,
-                'scale' => 2,
-                'attr' => ['class' => 'styled-input'],
-            ])
-            ->add('cout_minute', NumberType::class, [
-                'label' => false,
-                'required' => false,
-                'scale' => 2,
-                'attr' => ['class' => 'styled-input'],
-            ])
-            ->add('penalite', NumberType::class, [
-                'label' => false,
-                'required' => false,
-                'scale' => 2,
-                'attr' => ['class' => 'styled-input'],
-            ])
-             ->add('prix_collab_dc', NumberType::class, [
-                'mapped' => false,
-                'required' => false,
-                'label' => 'Tarif collaborateurs DC'
-            ])
-            ->add('prix_public_dc', NumberType::class, [
-                'mapped' => false,
-                'required' => false,
-                'label' => 'Tarif public DC'
-            ])
-            ->add('cout_minute_dc', NumberType::class, [
-                'mapped' => false,
-                'required' => false,
-                'label' => 'Coût à la minute DC'
-            ])
-            ->add('penalite_dc', NumberType::class, [
-                'mapped' => false,
-                'required' => false,
-                'label' => 'Pénalité post-recharge DC'
-            ]);
+        $acCount = $options['ac_count'] ?? 0;
+        $dcCount = $options['dc_count'] ?? 0;
+
+        if ($acCount > 0) {
+            $builder
+                ->add('prix_collab', NumberType::class, [
+                    'label' => 'Prix collaborateurs AC (€HT/kWh)',
+                    'required' => true,
+                    'scale' => 2,
+                    'attr' => ['class' => 'styled-input'],
+                ])
+                ->add('prix_public', NumberType::class, [
+                    'label' => 'Prix public AC (€HT/kWh)',
+                    'required' => false,
+                    'scale' => 2,
+                    'attr' => ['class' => 'styled-input'],
+                ])
+                ->add('cout_minute', NumberType::class, [
+                    'label' => 'Coût à la minute AC',
+                    'required' => false,
+                    'scale' => 2,
+                    'attr' => ['class' => 'styled-input'],
+                ])
+                ->add('penalite', NumberType::class, [
+                    'label' => 'Pénalité post-recharge AC',
+                    'required' => false,
+                    'scale' => 2,
+                    'attr' => ['class' => 'styled-input'],
+                ]);
+        }
+
+        if ($dcCount > 0) {
+            $builder
+                ->add('prix_collab_dc', NumberType::class, [
+                    'mapped' => false,
+                    'required' => false,
+                    'label' => 'Tarif collaborateurs DC',
+                    'attr' => ['class' => 'styled-input'],
+                ])
+                ->add('prix_public_dc', NumberType::class, [
+                    'mapped' => false,
+                    'required' => false,
+                    'label' => 'Tarif public DC',
+                    'attr' => ['class' => 'styled-input'],
+                ])
+                ->add('cout_minute_dc', NumberType::class, [
+                    'mapped' => false,
+                    'required' => false,
+                    'label' => 'Coût à la minute DC',
+                    'attr' => ['class' => 'styled-input'],
+                ])
+                ->add('penalite_dc', NumberType::class, [
+                    'mapped' => false,
+                    'required' => false,
+                    'label' => 'Pénalité post-recharge DC',
+                    'attr' => ['class' => 'styled-input'],
+                ]);
+        }
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
-        $resolver->setDefaults([]);
+        $resolver->setDefaults([
+            'data_class' => null,
+            'ac_count' => 0,
+            'dc_count' => 0,
+        ]);
     }
 }
